e52b7e28de1e85487906283900ab785f50d9619c
Vinod R <158383260+Vinod-R-07@users.noreply.github.com>
2026-06-02 17:32:21 +0530

    Update large_function.c

--- testcases/large_function.c
+++ testcases/large_function.c
@@ -1,1 +1,18 @@
+int compute(int n) {
+    int sum = 0;
 
+    for (int i = 0; i < n; i++) {
+        sum += i;
+        sum *= 2;
+        sum -= 1;
+        sum += 3;
+        sum *= 4;
+        sum -= 2;
+    }
+
+    return sum;
+}
+
+int main() {
+    return compute(100);
+}
