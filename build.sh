60f4836b9b2fe10e58f5320c55af9c65e2dd1b8f
Vinod R <vinodsaaho1@gmail.com>
2026-06-02 20:26:54 +0000

    Add working build and run scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+mkdir -p build
+cd build
+
+cmake .. -DLLVM_DIR=/usr/lib/llvm-18/lib/cmake/llvm
+make
--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+clang -S -emit-llvm testcases/small_function.c -o small_function.ll
+
+opt --load-pass-plugin ./build/libInlinerPass.so \
+    --passes=hello-world \
+    -disable-output \
+    small_function.ll
